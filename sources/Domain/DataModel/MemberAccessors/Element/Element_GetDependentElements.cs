8761a912f713061aa7eeb5a730fa864fe3127b37
NeVeSpl <nevespl@gmail.com>
2022-07-10 19:52:05 +0200

    add Accessor for: Element.GetDependentElements

---
+++ sources/Domain/DataModel/MemberAccessors/Element/Element_GetDependentElements.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+
+namespace RevitDBExplorer.Domain.DataModel.MemberAccessors
+{
+    internal class Element_GetDependentElements : IMemberAccessor, IHaveFactoryMethod
+    {
+        string IHaveFactoryMethod.TypeAndMemberName => "Element.GetDependentElements";
+        IMemberAccessor IHaveFactoryMethod.Create() => new Element_GetDependentElements();
+
+        public ReadResult Read(Document document, object @object)
+        {
+            return new ReadResult("[Element]", "IList<ElementId>", true);
+        }
+
+        public IEnumerable<SnoopableObject> Snooop(Document document, object @object)
+        {
+            var element = @object as Element;
+            var ids = element.GetDependentElements(null);
+            if  (ids.Any())
+            {
+                var elements = new FilteredElementCollector(document, ids).WhereElementIsNotElementType().ToElements();
+                return elements.Select(x => new SnoopableObject(x, document));
+            }
+
+            return Enumerable.Empty<SnoopableObject>();
+        }
+    }
+}
--- sources/Domain/DataModel/MemberAccessors/MemberAccessorFactory.cs
+++ sources/Domain/DataModel/MemberAccessors/MemberAccessorFactory.cs
@@ -12,12 +12,17 @@ namespace RevitDBExplorer.Domain.DataModel.MemberAccessors
         private static readonly IHaveFactoryMethod[] MemberAccessorFactories = new IHaveFactoryMethod[]
         {
             new Element_Geometry(),
+            new Element_GetDependentElements()
         };
         private static readonly Dictionary<string, IHaveFactoryMethod> MemberAccessorFactoriesLookup;
 
         static MemberAccessorFactory()
         {
-            MemberAccessorFactoriesLookup = MemberAccessorFactories.ToDictionary(x => x.TypeAndMemberName);
+            var type = typeof(IHaveFactoryMethod);
+            var types = type.Assembly.GetTypes().Where(p => type.IsAssignableFrom(p) && !p.IsInterface).ToList();
+            var memberAccessorFactories = types.Select(x => Activator.CreateInstance(x) as IHaveFactoryMethod);
+
+            MemberAccessorFactoriesLookup = memberAccessorFactories.ToDictionary(x => x.TypeAndMemberName);
         }
 
 
