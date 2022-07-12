7a5e5036fabc20efe22a67fbf3e887ab54e8f528
NeVeSpl <nevespl@gmail.com>
2022-07-12 17:54:19 +0200

    add Extensible storage accessor

---
+++ sources/Domain/DataModel/MemberAccessors/Element/Element_GetEntity.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.ExtensibleStorage;
+
+// (c) Revit Database Explorer https://github.com/NeVeSpl/RevitDBExplorer/blob/main/license.md
+
+namespace RevitDBExplorer.Domain.DataModel.MemberAccessors
+{
+    internal class Element_GetEntity : MemberAccessorByType<Element>, IHaveFactoryMethod
+    {
+        public override string MemberName => nameof(Element.GetEntity);
+        public override string MemberParams => typeof(Schema).Name;
+        IMemberAccessor IHaveFactoryMethod.Create() => new Element_GetEntity();       
+
+
+
+        protected override bool CanBeSnoooped(Document document, Element element)
+        {
+            foreach (var id in element.GetEntitySchemaGuids())
+            {
+                var schema = Schema.Lookup(id);
+                if(schema.ReadAccessGranted()) return true;
+            }
+            return false;
+        }
+        protected override string GetLabel(Document document, Element element)
+        {          
+            return "[Entity]";
+        }
+        protected override IEnumerable<SnoopableObject> Snooop(Document document, Element element)
+        {
+            var schemas = Schema.ListSchemas();
+
+            foreach (var schema in schemas)
+            {
+                if (!schema.ReadAccessGranted()) continue;
+                var entity = element.GetEntity(schema);
+                if (!entity.IsValid()) continue;
+
+                yield return new SnoopableObject(schema, document, null, new[] { new SnoopableObject(entity, document) });
+            }         
+        }       
+    }
+}
---
+++ sources/Domain/DataModel/MemberAccessors/Entity/Entity_Get.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.ExtensibleStorage;
+
+// (c) Revit Database Explorer https://github.com/NeVeSpl/RevitDBExplorer/blob/main/license.md
+
+
+namespace RevitDBExplorer.Domain.DataModel.MemberAccessors
+{
+    internal class Entity_Get : MemberAccessorByType<Entity>, IHaveFactoryMethod
+    {
+        public override string MemberName => nameof(Entity.Get);
+        public override string MemberParams => typeof(Field).Name;
+        IMemberAccessor IHaveFactoryMethod.Create() => new Entity_Get();
+
+
+
+        protected override bool CanBeSnoooped(Document document, Entity entity)
+        {
+            return entity.IsValid() && entity.Schema.ReadAccessGranted() && entity.Schema.ListFields().Count > 0;
+        }
+
+        protected override string GetLabel(Document document, Entity entity)
+        {
+            return "[Extensible storage values]";
+        }
+
+        protected override IEnumerable<SnoopableObject> Snooop(Document document, Entity entity)
+        {
+            var fields = entity.Schema.ListFields();
+            var getWithField = ((Func<Field, object>)entity.Get<object>).Method.GetGenericMethodDefinition();
+            var getWithFielAndUnit = ((Func<Field, ForgeTypeId, object>)entity.Get<object>).Method.GetGenericMethodDefinition();
+
+            List<SnoopableObject> snoopableFields = new List<SnoopableObject>(fields.Count);
+
+            foreach (var field in fields)
+            {
+                var fieldValueType = GetFieldValueType(field);
+                var fieldSpecType = field.GetSpecTypeId();
+                MethodInfo closedGenericGet = null;
+                object[] parameters = null;
+                if (UnitUtils.IsMeasurableSpec(fieldSpecType) || fieldSpecType == SpecTypeId.Custom)
+                {
+                    var unit = UnitUtils.IsMeasurableSpec(fieldSpecType) ? UnitUtils.GetValidUnits(fieldSpecType)[0] : UnitTypeId.Custom;
+                    closedGenericGet = getWithFielAndUnit.MakeGenericMethod(fieldValueType);
+                    parameters = new object[] { field, unit };
+                }
+                else
+                {
+                    closedGenericGet = getWithField.MakeGenericMethod(fieldValueType);
+                    parameters = new object[] { field };
+                }
+                var value = closedGenericGet.Invoke(entity, parameters);
+                yield return new SnoopableObject(value, document, $"Field: {field.FieldName}");
+            }
+        }
+
+        private Type GetFieldValueType(Field field)
+        {
+            return field.ContainerType switch
+            {
+                ContainerType.Simple => field.ValueType,
+                ContainerType.Array => typeof(IList<>).MakeGenericType(field.ValueType),
+                ContainerType.Map => typeof(IDictionary<,>).MakeGenericType(field.KeyType, field.ValueType),
+                _ => throw new NotSupportedException(),
+            };
+        }
+    }
+}
