16da3a612f508d8b5fc57c8e5addb602042a5f43
NeVeSpl <nevespl@gmail.com>
2022-07-18 09:43:18 +0200

    xaml refactoring

---
+++ sources/WPF/Converters/EnumToVisibilityConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace RevitDBExplorer.WPF.Converters
+{
+    internal class EnumToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var strValue = value?.ToString();
+            var strPar = parameter?.ToString(); 
+
+            if (String.Equals(strValue, strPar, StringComparison.OrdinalIgnoreCase))
+            {
+                return Visibility.Visible;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
