83a789c1f0deca88aa265f0a4b3a01600b04e4d2
NeVeSpl <nevespl@gmail.com>
2022-07-31 13:46:03 +0200

    expose  Document.GetDocumentVersion

---
+++ sources/Domain/DataModel/MemberTemplates/Document_Templates.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.DB;
+using RevitDBExplorer.Domain.DataModel.MemberTemplates.Base;
+
+// (c) Revit Database Explorer https://github.com/NeVeSpl/RevitDBExplorer/blob/main/license.md
+
+namespace RevitDBExplorer.Domain.DataModel.MemberTemplates
+{
+    internal class Document_Templates : IHaveMemberTemplates
+    {
+        private static readonly IEnumerable<ISnoopableMemberTemplate> templates = Enumerable.Empty<ISnoopableMemberTemplate>();
+
+        static Document_Templates()
+        {
+            templates = new ISnoopableMemberTemplate[]
+            {
+                SnoopableMemberTemplate<Document>.Create((doc, target) => Document.GetDocumentVersion(target), kind: SnoopableMember.Kind.StaticMethod),
+              
+            };
+        }
+
+
+        public IEnumerable<ISnoopableMemberTemplate> GetTemplates()
+        {
+            return templates;
+        }
+    }
+}
