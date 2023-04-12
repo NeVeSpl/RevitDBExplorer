b5055fd31f1da08a1fc62ce47e1153a80e5cb86b
NeVeSpl <nevespl@gmail.com>
2023-04-12 09:33:24 +0200

    expose ExternalService.GetServer()

---
+++ sources/Domain/DataModel/MemberAccessors/ExternalService/ExternalService_GetServer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.ExternalService;
+
+// (c) Revit Database Explorer https://github.com/NeVeSpl/RevitDBExplorer/blob/main/license.md
+
+namespace RevitDBExplorer.Domain.DataModel.MemberAccessors
+{
+    internal class ExternalService_GetServer : MemberAccessorByType<ExternalService>, ICanCreateMemberAccessor
+    {
+        IEnumerable<LambdaExpression> ICanCreateMemberAccessor.GetHandledMembers() { yield return (ExternalService x) => x.GetServer(new Guid()); }
+
+
+        protected override bool CanBeSnoooped(Document document, ExternalService externalService) => externalService.GetRegisteredServerIds().Any();
+        protected override string GetLabel(Document document, ExternalService externalService) => Labeler.GetLabelForCollection(nameof(ExternalService), externalService.GetRegisteredServerIds().Count);
+        protected override IEnumerable<SnoopableObject> Snooop(Document document, ExternalService externalService)
+        {
+            foreach (var serverId in externalService.GetRegisteredServerIds())
+            {
+                yield return new SnoopableObject(document, externalService.GetServer(serverId));
+            }
+        }
+    }
+}
