c7b3b469444533d3e3694690ddbd3dd06b6ce99f
NeVeSpl <nevespl@gmail.com>
2023-08-23 14:06:01 +0200

    replace ForgeTypeId.TypeId string value with the defined property on UnitTypeId, SpecTypeId, GroupTypeId, ParameterTypeId

--- sources/RevitDBExplorer/Domain/DataModel/ValueContainers/ForgeTypeIdHandler.cs
+++ sources/RevitDBExplorer/Domain/DataModel/ValueContainers/ForgeTypeIdHandler.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Autodesk.Revit.DB;
 using RevitDBExplorer.Domain.DataModel.ValueContainers.Base;
 
@@ -8,9 +9,51 @@ namespace RevitDBExplorer.Domain.DataModel.ValueContainers
 {
     internal sealed class ForgeTypeIdHandler : TypeHandler<ForgeTypeId>
     {
+        private static readonly Dictionary<string, string> typeIdToPropName = new Dictionary<string, string>();
+
+
+        static ForgeTypeIdHandler()
+        {
+            CacheForgeTypeIds(typeof(UnitTypeId));
+            CacheForgeTypeIds(typeof(SpecTypeId));
+#if R2022b
+            CacheForgeTypeIds(typeof(SpecTypeId.Boolean));
+            CacheForgeTypeIds(typeof(SpecTypeId.Int));
+            CacheForgeTypeIds(typeof(SpecTypeId.Reference));
+            CacheForgeTypeIds(typeof(SpecTypeId.String));
+            CacheForgeTypeIds(typeof(GroupTypeId));
+            CacheForgeTypeIds(typeof(ParameterTypeId));
+#endif
+        }
+        private static void CacheForgeTypeIds(Type  containerType)
+        {           
+            var props = containerType.GetProperties(System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+
+            var containerTypeName = containerType.Name;
+            if (containerType.IsNested)
+            {
+                containerTypeName = containerType.DeclaringType.Name + "." + containerType.Name;
+            }
+
+            foreach (var prop in props)
+            {
+                var forgeTypeId = prop.GetValue(null, null) as ForgeTypeId;
+                if (forgeTypeId != null)
+                {
+                    typeIdToPropName[forgeTypeId.TypeId] = $"{containerTypeName}.{prop.Name}";
+                }
+            }
+        }
+
+
         protected override bool CanBeSnoooped(SnoopableContext context, ForgeTypeId id) => id is not null;
         protected override string ToLabel(SnoopableContext context, ForgeTypeId id)
-        {           
+        { 
+            if (typeIdToPropName.TryGetValue(id.TypeId, out var propName))
+            {
+                return propName;
+            }
+
             return $"{id.TypeId}";
         }
         protected override IEnumerable<SnoopableObject> Snooop(SnoopableContext context, ForgeTypeId id)
