33f96d0333d4497fcf40f09f67877d9790d0813a
NeVeSpl <nevespl@gmail.com>
2024-04-15 10:41:00 +0200

    add handler for IndependentTag.TagText

---
+++ sources/RevitDBExplorer/Domain/DataModel/MemberAccessors/IndependentTag/IndependentTag_TagText.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+
+// (c) Revit Database Explorer https://github.com/NeVeSpl/RevitDBExplorer/blob/main/license.md
+
+namespace RevitDBExplorer.Domain.DataModel.MemberAccessors
+{
+    internal class IndependentTag_TagText : MemberAccessorByType<IndependentTag>, ICanCreateMemberAccessor
+    {
+        IEnumerable<LambdaExpression> ICanCreateMemberAccessor.GetHandledMembers()
+        {
+            yield return (IndependentTag x) => x.TagText;            
+        }      
+        
+
+        protected override bool CanBeSnoooped(Document document, IndependentTag value) => false;
+        protected override string GetLabel(Document document, IndependentTag value)
+        {
+#if R2025b
+            if (RebarBendingDetail.IsBendingDetail(value))
+            {
+                return string.Empty;
+            }
+#endif
+
+            return value.TagText;
+        }
+    }
+}
