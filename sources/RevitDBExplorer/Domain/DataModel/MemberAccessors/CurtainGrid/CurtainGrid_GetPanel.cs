75f4e392162611b3a7f91510b40c81b3ef99d1c9
NeVeSpl <nevespl@gmail.com>
2024-04-29 19:39:15 +0200

    expose CurtainGrid.GetPanel and CurtainGrid.GetCell

---
+++ sources/RevitDBExplorer/Domain/DataModel/MemberAccessors/CurtainGrid/CurtainGrid_GetPanel.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using Autodesk.Revit.DB;
+
+// (c) Revit Database Explorer https://github.com/NeVeSpl/RevitDBExplorer/blob/main/license.md
+
+namespace RevitDBExplorer.Domain.DataModel.MemberAccessors
+{
+    internal class CurtainGrid_GetPanel : MemberAccessorByType<CurtainGrid>, ICanCreateMemberAccessor
+    {
+        IEnumerable<LambdaExpression> ICanCreateMemberAccessor.GetHandledMembers() { yield return (CurtainGrid x) => x.GetPanel(ElementId.InvalidElementId, ElementId.InvalidElementId); }
+
+
+        protected override bool CanBeSnoooped(Document document, CurtainGrid grid) => true;
+        protected override string GetLabel(Document document, CurtainGrid grid)
+        {      
+            string value = $"[Panel]";
+            return value;
+        }
+        protected override IEnumerable<SnoopableObject> Snooop(Document document, CurtainGrid grid)
+        {
+            var uLineIds = grid.GetUGridLineIds();
+            var vLineIds = grid.GetVGridLineIds();
+            uLineIds.Add(ElementId.InvalidElementId);
+            vLineIds.Add(ElementId.InvalidElementId);
+
+            foreach (var uLineId in uLineIds)
+            {
+                foreach (var vLineId in vLineIds)
+                {
+                    var cell = grid.GetPanel(uLineId, vLineId);
+
+                    yield return new SnoopableObject(document, cell) { Name = $"uGridLineId: {uLineId}, vGridLineId: {vLineId}" };
+                }
+            }
+        }
+    }
+}
