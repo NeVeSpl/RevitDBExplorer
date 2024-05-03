408160f35c9726b500310fec47af78df8383e562
mostafa901 <mostafa90@gmail.com>
2024-05-03 04:36:01 +0200

    add split volumes

---
+++ sources/RevitDBExplorer/Domain/DataModel/MemberTemplates/Solid_Templates.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.DB;
+using RevitDBExplorer.Domain.DataModel.MemberTemplates.Base;
+using RevitDBExplorer.Domain.DataModel.Streams;
+using RevitDBExplorer.Domain.DataModel.Streams.Base;
+
+// (c) Revit Database Explorer https://github.com/NeVeSpl/RevitDBExplorer/blob/main/license.md
+
+namespace RevitDBExplorer.Domain.DataModel.MemberTemplates
+{
+    internal class Solid_Templates : IHaveMemberTemplates
+    {
+        private static readonly IEnumerable<ISnoopableMemberTemplate> templates = Enumerable.Empty<ISnoopableMemberTemplate>();
+
+        static Solid_Templates()
+        {
+            templates = new ISnoopableMemberTemplate[]
+            {
+               SnoopableMemberTemplate<Solid>.Create((document, target) => SolidUtils.SplitVolumes(target), kind: MemberKind.StaticMethod),
+            };
+        }
+
+        public IEnumerable<ISnoopableMemberTemplate> GetTemplates()
+        {
+            return templates;
+        }
+    }
+}
