269757b0f9da9cf2cc30d9f1f838edfa2bd777c4
mostafa901 <mostafa90@gmail.com>
2024-05-08 06:27:39 +0200

    Added Adaptive component Instance Util

---
+++ sources/RevitDBExplorer/Domain/DataModel/MemberTemplates/FamilyInstance_Templates.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Autodesk.Revit.DB;
+using RevitDBExplorer.Domain.DataModel.MemberTemplates.Base;
+using RevitDBExplorer.Domain.DataModel.Streams;
+using RevitDBExplorer.Domain.DataModel.Streams.Base;
+
+// (c) Revit Database Explorer https://github.com/NeVeSpl/RevitDBExplorer/blob/main/license.md
+
+namespace RevitDBExplorer.Domain.DataModel.MemberTemplates
+{
+    internal class FamilyInstance_Templates : IHaveMemberTemplates
+    {
+        private static readonly IEnumerable<ISnoopableMemberTemplate> templates = Enumerable.Empty<ISnoopableMemberTemplate>();
+
+        static FamilyInstance_Templates()
+        {
+            templates = new ISnoopableMemberTemplate[]
+            {
+               SnoopableMemberTemplate<FamilyInstance>.Create((doc, target) => AdaptiveComponentInstanceUtils.GetInstancePlacementPointElementRefIds(target), kind: MemberKind.AsArgument),
+                
+            }; 
+        }
+
+        public IEnumerable<ISnoopableMemberTemplate> GetTemplates()
+        {
+            return templates;
+        }
+    }
+}
