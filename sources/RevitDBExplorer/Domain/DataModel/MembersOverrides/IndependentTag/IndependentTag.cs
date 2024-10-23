c4d15b3572625aab12a9c6f16d25a69669e0e7cc
NeVeSpl <nevespl@gmail.com>
2024-10-23 12:43:16 +0200

    fix build

--- sources/RevitDBExplorer/Domain/DataModel/MembersOverrides/IndependentTag/IndependentTag.cs
+++ sources/RevitDBExplorer/Domain/DataModel/MembersOverrides/IndependentTag/IndependentTag.cs
@@ -11,9 +11,11 @@ namespace RevitDBExplorer.Domain.DataModel.MembersOverrides
     {
         public IEnumerable<IMemberOverride> GetOverrides() =>
         [
+#if R2022_MIN
             MemberOverride<IndependentTag>.ByFuncWithParam((document, element, referenceTagged) => element.GetLeaderElbow(referenceTagged), (document, element) => element.GetTaggedReferences()),
             MemberOverride<IndependentTag>.ByFuncWithParam((document, element, referenceTagged) => element.GetLeaderEnd(referenceTagged), (document, element) => element.GetTaggedReferences()),
             MemberOverride<IndependentTag>.ByFuncWithParam((document, element, referenceTagged) => element.HasLeaderElbow(referenceTagged), (document, element) => element.GetTaggedReferences()),
+#endif
 
 #if R2023_MIN
             MemberOverride<IndependentTag>.ByFuncWithParam((document, element, referenceTagged) => element.IsLeaderVisible(referenceTagged), (document, element) => element.GetTaggedReferences()),
